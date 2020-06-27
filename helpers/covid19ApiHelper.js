23a1d5752fe905c252f79987bb0700dfbaf6ac27
Chandrakanth Rajesh <chandrakanthr789@gmail.com>
2020-06-27 05:05:19 -0400

    updated the api helper

---
+++ helpers/covid19ApiHelper.js
@@ -0,0 +1,37 @@
+let helper = {
+    let slugDetails = null;
+    constuctor = () => {
+        loadSlugDetails();
+    }
+    getAllDetailsForCountry = (cc, cb) => {
+        let slug = getSlugForCC(cc);
+        fetch('https://api.covid19api.com/total/country/{{slug}}')
+
+        // using route  get details
+            // return the details
+
+        .then(response => response.json())
+        .then(data => console.log(data));
+        
+    }
+
+    getSlugForCC = (cc) => {
+        // check slugDetails for cc
+        fetch('https://api.covid19api.com/countries')
+        .then(res => res.json())
+        .then(data => console.log(data.Slug))
+        return(data.Slug);
+
+        // return slug
+    }
+
+    loadSlugDetails = () => {
+        // call route "https://api.covid19api.com/countries"
+        fetch('https://api.covid19api.com/countries')
+        // slugDetails = values;
+        .then(res => res.json())
+        .then(data => console.log(data) )
+        return data;
+    }
+
+}
