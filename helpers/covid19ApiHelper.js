84788ec7646834aebd7b6f9f16deb0c044b594ea
Chandrakanth Rajesh <chandrakanthr789@gmail.com>
2020-06-28 21:03:25 -0400

    Wrote the logic to find slug for the given IS02

--- helpers/covid19ApiHelper.js
+++ helpers/covid19ApiHelper.js
@@ -1,38 +1,46 @@
 let helper = {
     let slugDetails = null;
+    let countries = null;
     constuctor = () => {
         loadSlugDetails();
     }
-    getAllDetailsForCountry = (cc, cb) => {
-        let slug = getSlugForCC(cc);
+    getAllDetailsForCountry = (Iso2, cb) => {
+        let slug = getSlugForIso2(Iso2);
         fetch('https://api.covid19api.com/total/country/{{slug}}')
 
         // using route  get details
             // return the details
 
         .then(response => response.json())
-        .then(data => console.log(data));
-        return(data);
+        .then(data => cb(data));
         
-    }
+    };
 
-    getSlugForCC = (cc) => {
+    getSlugForIso2 = (Iso2) => {
+        //find the slug for country code
         // check slugDetails for cc
+        let Slug = null;
+        
         fetch('https://api.covid19api.com/countries')
         .then(res => res.json())
         .then(data => console.log(data.Slug))
+        for(let i=0:i<=data; i++)
+        if(i === "Slug");
+        console.log(i);
+
         return(data.Slug);
 
         // return slug
-    }
+    };
 
     loadSlugDetails = () => {
         // call route "https://api.covid19api.com/countries"
         fetch('https://api.covid19api.com/countries')
         // slugDetails = values;
         .then(res => res.json())
-        .then(data => console.log(data) )
-        return data;
-    }
+        .then(data => {
+            countries = data;
+        });
+    };
 
 }
